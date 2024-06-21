2234e5dfe6604718ff365e70001a48340c60a66b
samson28 <sbadayodi@gmail.com>
2024-06-21 01:13:31 +0000

    hehe

--- src/main/java/com/lundu/hello/HelloController.java
+++ src/main/java/com/lundu/hello/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @RequestMapping("/")
     public @ResponseBody String greeting() {
-        return "Hello, THIS IS AN AMAZING TUTORIEL";
+        return "Hello, WORLD";
     }
 }
